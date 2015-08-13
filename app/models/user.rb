82150cbf82f28d415c447cf152f72072bb951ff7
armstrhb <henry.b.armstrong@gmail.com>
2015-08-12 21:26:08 -0400

    User model (new gem: bcrypt)

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  #TODO: has_many :item_history, :item_property_history
+  has_secure_password
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: true
+  validates :password, length: { minimum: 6 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812014129) do
+ActiveRecord::Schema.define(version: 20150813010325) do
 
   create_table "areas", force: :cascade do |t|
     t.string   "name",        null: false
@@ -48,6 +48,14 @@ ActiveRecord::Schema.define(version: 20150812014129) do
     t.integer  "item_type_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",                            null: false
+    t.string   "password_digest"
+    t.boolean  "admin",           default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
   end
 
 end
