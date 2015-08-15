5a9009b037a05c874633d4a636e0d64518038e2b
armstrhb <henry.b.armstrong@gmail.com>
2015-08-15 15:11:33 -0400

    Item unit tests: area presence
    Items must contain an item type to be saved.

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -8,4 +8,12 @@ class ItemTest < ActiveSupport::TestCase
     item.area = areas(:area_one)
     assert item.save, "item save failed with area"
   end
+
+  test "Item: must contain an item type" do
+    item = Item.new(:area => areas(:area_one))
+    assert_not item.save, "item saved without an item type"
+
+    item.item_type = item_types(:cpu)
+    assert item.save, "item save failed with item type"
+  end
 end
