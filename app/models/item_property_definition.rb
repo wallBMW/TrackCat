e95cfe41b870549d2c38bdc690885856488afff6
armstrhb <henry.b.armstrong@gmail.com>
2015-08-15 15:44:53 -0400

    Item Property Definition unit tests: name
    name should be between 2 and 50 characters. let's test if it's true!

--- app/models/item_property_definition.rb
+++ app/models/item_property_definition.rb
@@ -1,4 +1,5 @@
 class ItemPropertyDefinition < ActiveRecord::Base
   belongs_to :item_type, :foreign_key => "item_type_id"
-  validates :name, uniqueness: { scope: :item_type_id }, length: { minimum: 2, maximum: 255 }
+
+  validates :name, uniqueness: { scope: :item_type_id }, length: { minimum: 2, maximum: 50 }
 end
--- test/models/item_property_definition_test.rb
+++ test/models/item_property_definition_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ItemPropertyDefinitionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Item Property Definition: name must be between 2 and 50 characters" do
+    prop_def = ItemPropertyDefinition.new(:item_type => item_types(:cpu))
+    assert_not prop_def.save, "saved with nil name"
+
+    prop_def.name = "a"
+    assert_not prop_def.save, "saved with short name"
+
+    prop_def.name = "ab"
+    assert prop_def.save, "save failed with valid (2 char) name"
+
+    prop_def.name = "Carajillo percolator, grinder sit espresso trifect"
+    assert prop_def.save, "save failed with valid (50 char) name"
+
+    prop_def.name = "Carajillo percolator, grinder sit espresso trifecta"
+    assert_not prop_def.save, "saved with long name"
+  end
 end
