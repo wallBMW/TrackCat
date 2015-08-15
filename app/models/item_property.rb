2ae77f930190f67464a359be3a39ab06fd6437fc
armstrhb <henry.b.armstrong@gmail.com>
2015-08-15 16:09:37 -0400

    Item Property unit tests
    Item Properties should have an owning Item, and a referenced Item
    Property Definition. Value may be null.

--- app/models/item_property.rb
+++ app/models/item_property.rb
@@ -1,4 +1,7 @@
 class ItemProperty < ActiveRecord::Base
   belongs_to :item, :foreign_key => "item_id"
   belongs_to :item_property_definition, :foreign_key => "item_property_definition_id"
+
+  validates :item, presence: true
+  validates :item_property_definition, presence: true
 end
--- test/models/item_property_test.rb
+++ test/models/item_property_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class ItemPropertyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Item Property: owning item should be present" do
+    prop = ItemProperty.new(:item_property_definition => item_property_definitions(:cpu_brand), :value => "INTEL")
+    assert_not prop.save, "property saved with no owning item"
+
+    prop.item = items(:item_two)
+    assert prop.save, "property save failed with item present"
+  end
+
+  test "Item Property: definition must be present" do
+    prop = ItemProperty.new(:item => items(:item_two), :value => "Latte")
+    assert_not prop.save, "property saved with no definition"
+
+    prop.item_property_definition = item_property_definitions(:cpu_brand)
+    assert prop.save, "property save failed with definition present"
+  end
 end
