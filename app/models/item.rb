74a37e3dc96902a9c795e82d9e62a0fd08c3fde1
armstrhb <henry.b.armstrong@gmail.com>
2015-08-15 22:39:42 -0400

    fleshing out item prop history
    Also added item history and item property history to item.

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ActiveRecord::Base
   belongs_to :area, :foreign_key => "area_id"
   belongs_to :item_type, :foreign_key => "item_type_id"
   has_many :item_properties, :foreign_key => "item_id"
+  has_many :item_histories, :foreign_key => "item_id"
+  has_many :item_property_histories, :foreign_key => "item_id"
 
   validates :area, presence: true
   validates :item_type, presence: true
--- db/migrate/20150816015448_create_item_property_histories.rb
+++ db/migrate/20150816015448_create_item_property_histories.rb
@@ -1,7 +1,11 @@
 class CreateItemPropertyHistories < ActiveRecord::Migration
   def change
     create_table :item_property_histories do |t|
-
+      t.belongs_to :item, null: false
+      t.belongs_to :item_property_definition, null: false
+      t.string :old_value
+      t.string :new_value
+      t.belongs_to :user, null: false
       t.timestamps null: false
     end
   end
