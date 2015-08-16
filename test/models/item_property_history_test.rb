9202eeac1c6781e546c5b730c90fde83014dcb47
armstrhb <henry.b.armstrong@gmail.com>
2015-08-15 23:20:16 -0400

    item property history UT: item presence
    item property must be present when saving an item property history.

--- test/models/item_property_history_test.rb
+++ test/models/item_property_history_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ItemPropertyHistoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "ItemPropertyHistory: item must be present" do
+    item = items(:item_one)
+    prop = item.item_properties.where(:value => "INTEL").first
+
+    hist = ItemPropertyHistory.new(:item_property_definition => prop.item_property_definition)
+    hist.user = users(:armstrhb)
+    hist.old_value = prop.value
+
+    prop.value = "AMD"
+    hist.new_value = prop.value
+
+    assert_not hist.save, "item property history saved with no item"
+
+    hist.item = item
+    assert hist.save, "item property history not saved with item present"
+  end
 end
