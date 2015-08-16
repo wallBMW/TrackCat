9db9dada713832a89c6c77e99b0b5d8a6e6dfb3e
Brandon Wall <brandonwall93@gmail.com>
2015-08-16 00:55:14 -0400

    item history test: verify new area

--- test/models/item_history_test.rb
+++ test/models/item_history_test.rb
@@ -4,4 +4,18 @@ class ItemHistoryTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "ItemHistory: verify new area" do
+    item = items(:item_one)
+    hist = ItemHistory.new
+
+    hist.user = users(:wallBMW)
+    hist.old_area = item.area
+
+    item.area = areas(:area_three)
+    hist.new_area = item.area
+
+    assert item.save, "item saved with updated area"
+    assert hist.save, "history saved with updated area"
+    end
 end
