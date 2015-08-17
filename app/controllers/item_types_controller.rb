aa8ff9fe257ad471f72238e3e9c8ba031a7b8af6
armstrhb <henry.b.armstrong@gmail.com>
2015-08-16 22:12:34 -0400

    item type controller index, unit tests

--- app/controllers/item_types_controller.rb
+++ app/controllers/item_types_controller.rb
@@ -1,2 +1,20 @@
 class ItemTypesController < ApplicationController
+  def index
+    @item_types = ItemType.all
+  end
+
+  def create
+    @item_type = ItemType.new(create_params)
+
+    if @item_type.save
+      @item_type
+    else
+      render :json => {:errors => @item_type.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def create_params
+      params.require(:item_type).permit(:name, :description)
+    end
 end
--- test/controllers/item_types_controller_test.rb
+++ test/controllers/item_types_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ItemTypesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "/itemtypes should route to item_types index" do
+    assert_routing "/itemtypes", { controller: "item_types", action: "index" }
+  end
+
+  test "/itemtypes should contain list of all item types" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:item_types)
+  end
 end
