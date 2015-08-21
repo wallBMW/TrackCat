ad95b3ede8ed9f5a9fa7065d0d38bf30ab17146b
armstrhb <henry.b.armstrong@gmail.com>
2015-08-20 22:43:15 -0400

    search view results, first pass
    todo: handle hard errors (500's) gracefully in view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,15 @@ class SearchController < ApplicationController
   end
 
   def search
+    @item_properties = ItemProperty.where(ItemProperty.arel_table[:value].matches("%#{params[:criteria]}%"))
+
+    @item_types = ItemType.where(ItemType.arel_table[:name].matches("%#{params[:criteria]}%"))
+    @item_types += ItemType.where(ItemType.arel_table[:description].matches("%#{params[:criteria]}%"))
+    @item_types = @item_types.uniq
+
+    @areas = Area.where(Area.arel_table[:name].matches("%#{params[:criteria]}%"))
+    @areas += Area.where(Area.arel_table[:description].matches("%#{params[:criteria]}%"))
+    @areas += Area.where(Area.arel_table[:location].matches("%#{params[:criteria]}%"))
+    @areas = @areas.uniq
   end
 end
